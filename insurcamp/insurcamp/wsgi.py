20c05675d8b3d773fc0262bf5f6cecc98eaad80d
pedropirescg <pedropires@discente.ufg.br>
2021-04-28 22:57:29 -0300

    Atualizando estrutura de códigos

---
+++ insurcamp/insurcamp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for insurcamp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insurcamp.settings')
+
+application = get_wsgi_application()
---
+++ insurcamp/mainservice/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MainserviceConfig(AppConfig):
+    name = 'mainservice'
